8b679c4edb326e9852f4b3f321d05ec6b9fa067b
aniaw <ania_w@outlook.com>
2016-05-24 23:04:51 +0200

    exercise3

---
+++ test/unit/BrainCandyDetailsCtrl.spec.js
@@ -0,0 +1,59 @@
+describe("BrainCandyDetailsCtrl", function () {
+    'use strict';
+    beforeEach(module('exerciseApp'));
+    var brainCandyDetailsCtrl;
+    var CandyDAOmock;
+
+    beforeEach(function () {
+        CandyDAOmock = jasmine.createSpyObj('CandyDAO', ['get', 'save', 'delete']);
+        CandyDAOmock.get.andReturn({
+            then: function (callback) {
+                callback({
+                    id: 0,
+                    name: '',
+                    factory: ''
+                });
+                return this;
+            }
+        })
+
+    });
+    beforeEach(inject(function ($controller) {
+        brainCandyDetailsCtrl = $controller('BrainCandyDetailsCtrl', {CandyDAO: CandyDAOmock})
+
+    }));
+
+    describe("CandyDAO.get()", function () {
+        it("should be called", function () {
+            expect(CandyDAOmock.get).toHaveBeenCalled();
+        });
+    });
+
+    describe("variable  details", function () {
+        it("should exist", function () {
+            expect(brainCandyDetailsCtrl.details).not.toBe(undefined);
+        });
+
+        it("should be a object", function () {
+            expect('object' == typeof brainCandyDetailsCtrl.details).toBe(true);
+        });
+        it("should heave properties : id, name, factory", function () {
+            expect(brainCandyDetailsCtrl.details['id']).not.toBe(undefined);
+            expect(brainCandyDetailsCtrl.details['name']).not.toBe(undefined);
+            expect(brainCandyDetailsCtrl.details['factory']).not.toBe(undefined);
+        });
+
+    });
+    describe("saveCandy()", function () {
+        it("should call CandyDAO.save()", function () {
+            brainCandyDetailsCtrl.saveCandy();
+            expect(CandyDAOmock.save).toHaveBeenCalled();
+        });
+    });
+    describe("deleteCandy()", function () {
+        it("should call CandyDAO.delete()", function () {
+            brainCandyDetailsCtrl.deleteCandy();
+            expect(CandyDAOmock.delete).toHaveBeenCalled();
+        });
+    });
+});
---
+++ test/unit/BrainCandyListCtrl.spec.js
@@ -0,0 +1,59 @@
+describe("BrainCandyListCtrl", function () {
+    'use strict';
+    beforeEach(module('exerciseApp'));
+    var brainCandyListCtrl;
+    var CandyDAOmock;
+    var CandyDAO_mock;
+    var sequence = 1;
+    var candies = {};
+    [
+        {
+            id: sequence++,
+            name: 'Krowka',
+            factory: 'Wawel'
+        },
+        {
+            id: sequence++,
+            name: 'Tiki-taki',
+            factory: 'Wawel'
+        },
+        {
+            id: sequence++,
+            name: 'Michalki',
+            factory: 'Wawel'
+        }
+    ].every(function (value) {
+        candies[value.id] = value;
+        return true;;
+    });
+    beforeEach(function (arg) {
+        CandyDAOmock = jasmine.createSpyObj('CandyDAO', ['query']);
+        CandyDAOmock.query.andReturn({
+            then: function (callback) {
+                callback(candies);
+                return this
+            }
+        });
+    });
+    beforeEach(inject(function ($controller) {
+        brainCandyListCtrl = $controller('BrainCandyListCtrl', {CandyDAO: CandyDAOmock})
+
+    }));
+
+    describe("CandyDAO.query()", function () {
+        it("should be called", function () {
+            expect(CandyDAOmock.query).toHaveBeenCalled();
+        });
+    });
+
+    describe("variable  list", function () {
+        it("should exist", function () {
+            expect(brainCandyListCtrl.list).not.toBe(undefined);
+        });
+
+        it("should be a object", function (test) {
+            expect('object' == typeof brainCandyListCtrl.list).toBe(true);
+        });
+
+    });
+});
